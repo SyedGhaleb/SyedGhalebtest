7f3fffe9467d833bdc1dcc7cc8a830392ca5b4c0
ghalib <syedghalib785@gmail.com>
2023-01-05 21:59:00 +0530

    3rd try

--- src/Package1/Testing1.java
+++ src/Package1/Testing1.java
@@ -5,7 +5,11 @@ public class Testing1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		print hello
+		print hello;
+		
+		print helllo
+		
+		print 2222222
 	}
 
 }
