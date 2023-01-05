0543c6b825de0c81c8a83c5b1a17ddd972eada18
ghalib <syedghalib785@gmail.com>
2023-01-05 22:02:43 +0530

    branch

--- src/Package1/Test3.java
+++ src/Package1/Test3.java
@@ -1,17 +1,11 @@
 package Package1;
 
-public class Testing1 {
+public class Test3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		print hello;
-		
-		print helllo
-		
-		print 2222222
-		
-		print hhhshshhs
+		braching 
 	}
 
 }
